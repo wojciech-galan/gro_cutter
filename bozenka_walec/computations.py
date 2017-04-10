1c3c99983e308e8273b6c0e05701d6e38554ecbf
wojciech-galan <wojciech.galan@gmail.com>
2017-04-10 09:18:50 +0200

    demo version ready

--- bozenka_walec/computations.py
+++ bozenka_walec/computations.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import math
+import functools
 import numpy as np
 from scipy.optimize import minimize
 
@@ -16,42 +17,48 @@ def squared_distance2d((x1, y1), (x2, y2)):
     return (x1 - x2) ** 2 + (y1 - y2) ** 2
 
 
-def distance_from_circle(point, center, radius, distance_f):
+def distance_from_circle(point, center, radius, squared=True):
     '''Computes distance between point and a circle'''
-    return math.fabs(radius-distance_f(point, center))
+    if squared:
+        return math.fabs(radius**2-squared_distance2d(point, center))
+    return math.fabs(radius - distance2d(point, center))
 
 
-def cumulative_distance_from_circle(points, center, radius, distance_f=squared_distance2d):
-    return sum(distance_from_circle(point, center, radius, distance_f) for point in points)
+def cumulative_distance_from_circle(points, center, radius, squared):
+    return sum(distance_from_circle(point, center, radius, squared) for point in points)
 
 
-def determine_center_and_radius(points, distance_f):
+def determine_center_and_radius(points):
     initialx = np.mean(points[:,0])
     initialy = np.mean(points[:,1])
     initial_radius = (math.fabs(initialx) + math.fabs(initialy) - np.min(points[:,0]) - np.min(points[:,1]))/2
     print initialx, initialy, initial_radius
-    def cumulative_distance_from_circle_wraper(x, distance_func=distance_f):
-        return cumulative_distance_from_circle(points, (x[0], x[1]), x[2], distance_f=distance_func)
-    res = minimize(cumulative_distance_from_circle_wraper, [initialx, initialy, initial_radius], method='nelder-mead',
+    def cumulative_distance_from_circle_wraper(x, squared):
+        return cumulative_distance_from_circle(points, (x[0], x[1]), x[2], squared)
+    res1 = minimize(functools.partial(cumulative_distance_from_circle_wraper, squared=True),
+                    [initialx, initialy, initial_radius], method='nelder-mead',
                    options={'xtol': 1e-8, 'disp': True})
-    # import functools
-    # res2 = minimize(functools.partial(cumulative_distance_from_circle_wraper, distance_func=distance2d), [initialx, initialy, initial_radius], method='nelder-mead',
-    #                options={'xtol': 1e-8, 'disp': True})
-    # print res.x
-    # print res2.x
-    # from matplotlib import pyplot as plt
-    # circle = plt.Circle((res.x[0], res.x[1]), res.x[2]/2, fill=False)
-    # circle2 = plt.Circle((res2.x[0], res2.x[1]), res2.x[2] / 2, fill=False)
-    # fig, ax = plt.subplots()
-    # plt.axis([-1, 5, -3, 3])
-    # ax.add_artist(circle)
-    # ax.add_artist(circle2)
-    # plt.plot(points[:,0], points[:,1])
-    # plt.show()
-    return res.x
+    res2 = minimize(functools.partial(cumulative_distance_from_circle_wraper, squared=False),
+                    [initialx, initialy, initial_radius], method='nelder-mead',
+                   options={'xtol': 1e-8, 'disp': True})
+    print res1.x
+    print res2.x
+    from matplotlib import pyplot as plt
+    circle = plt.Circle((res1.x[0], res1.x[1]), res1.x[2], fill=False, edgecolor='r', label='squared')
+    circle2 = plt.Circle((res2.x[0], res2.x[1]), res2.x[2], fill=False, edgecolor='g', label='not squared')
+    circle3 = plt.Circle((initialx, initialy), initial_radius, fill=False, edgecolor='c', label='before optimisation')
+    fig, ax = plt.subplots()
+    plt.axis([0, 16, 0, 16])
+    ax.add_artist(circle)
+    ax.add_artist(circle2)
+    ax.add_artist(circle3)
+    orig, = plt.plot(points[:,0], points[:,1], c='y', label='original data')
+    ax.legend(handles=[circle, circle2, circle3, orig])
+    plt.show()
+    return res1.x
 
 
 if __name__ == '__main__':
     #p = [(0, 0), (1, 1), (2, 0), (1, -1), (1+math.sqrt(2)/2, math.sqrt(2)/2), (1-math.sqrt(2)/2, -math.sqrt(2)/2)]
     p = np.array([(2+math.sin(x*math.pi/180), 2*math.cos(x*math.pi/180)) for x in range(360)])
-    print determine_center_and_radius(p, squared_distance2d)
+    print determine_center_and_radius(p)
--- bozenka_walec/walec.py
+++ bozenka_walec/walec.py
@@ -5,11 +5,63 @@
 # NANODYSK W PŁASZCZYŹNIE xy
 
 import math
+import os
+import argparse
+import numpy as np
 from string import digits
+from computations import determine_center_and_radius
+#            set(['POPC', 'GLN', 'TIP3', 'GLY', 'GLU', 'ASP', 'SER', 'HSD', 'LYS', 'PRO', 'ASN', 'VAL', 'THR', 'CLA',
+#            'TRP', 'SOD', 'PHE', 'ALA', 'MET', 'LEU', 'ARG', 'TYR'])
+AMINOACIDS = set(['GLN', 'GLY', 'GLU', 'ASP', 'SER', 'HSD', 'LYS', 'PRO', 'ASN', 'VAL', 'THR',
+             'TRP', 'PHE', 'ALA', 'MET', 'LEU', 'ARG', 'TYR'])
+# POPC TIP3 CLA SOD
+
+class DatFrame(object):
+
+    def __init__(self, frame_string):
+        super(DatFrame, self).__init__()
+        temp = frame_string.strip().split(os.linesep, 2)
+        self.first_two_lines = temp[:2]
+        content, self.last_line = temp[-1].rsplit(os.linesep, 1)
+        self.lines = [(x[:8].strip(), x[8:15].strip(), x[15:20].strip(), float(x[20:28]), float(x[28:36]), x[36:].strip())
+                      for x in content.split(os.linesep)] #TODO correct it! gro format description
+
+    def process(self):
+        points = []
+        for line in self.lines:
+            if line[0][-3:] in AMINOACIDS:
+                points.append((line[3], line[4]))
+        determine_center_and_radius(np.array(points))
 
 
+def process_frame(frame_string):
+    temp = frame_string.strip().split(os.linesep, 2)
+    first_two_lines = temp[:2]
+    last_line = temp[-1].rsplit(os.linesep, 1)[1]
+    print first_two_lines
+    print last_line
+    print [temp[2][:30]]
 
-cont = []
-for line in open('data/ramka.gro').readlines()[2:-1]:
-    cont.append(line.split(None, 1)[0].translate(None, digits))
-print cont[0], cont[-1]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='todo') # todo
+    parser.add_argument('-i', help='input file')
+    parser.add_argument('-o', help='output file')
+    parser.add_argument('-s', '--solvent', default='TIP3', help='solvent')
+    parser.add_argument('-k', '--skip', action='append', help='particles to skip')
+    parser.add_argument('-c', '--contain', default='POPC',
+                        help='particles to be contained in the output file without changes. '
+                             'AminoAcis are always contained')
+    args = parser.parse_args()
+    print args
+
+    if not os.path.exists(os.path.dirname(args.o)):
+        os.makedirs(os.path.dirname(args.o))
+    data = DatFrame(open(args.i).read())
+    data.process()
+    #process_frame(open(args.i).read())
+
+    # cont = []
+    # for line in open('data/ramka.gro').readlines()[2:-1]:
+    #     cont.append(line.split(None, 1)[0].translate(None, digits))
+    # print set(cont)
