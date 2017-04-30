d42602a693b801d83b120ccf436b5427644355e0
wojciech-galan <wojciech.galan@gmail.com>
2017-04-30 12:01:08 +0200

    additional option - displays fitting circle to the protein atoms in the first frame

--- bozenka_walec/__main__.py
+++ bozenka_walec/__main__.py
@@ -5,12 +5,18 @@ import os
 import sys
 import argparse
 import multiprocessing
+import math
+import numpy as np
+
 import common
-from walec import get_frames, DataFrame, process_frame_string, write_file
+import plot
+from walec import get_frames, DataFrame, process_frame_string, write_file, get_protein_atoms, \
+    determine_center_and_radius
 
 
 def main(args=sys.argv[1:]):
     parser = argparse.ArgumentParser(description=common.package_description)
+    parser.add_argument('-f', '--figure', action='store_true', help='plots circle fitted to the first frame')
     parser.add_argument('-i', required=True, help='input file')
     parser.add_argument('-o', required=True, help='output file')
     parser.add_argument('-s', '--solvent', default='TIP3', help='solvent')
@@ -27,36 +33,51 @@ def main(args=sys.argv[1:]):
     parser.add_argument('-p', '--processes', default=0, type=int, help="Number of additional cores used for \
                             computations. Values >=2 and <=num_of_available_cores are reasonable")
     args = parser.parse_args(args)
-    print "Num of cores:", 1 + args.processes
+
     if os.path.dirname(args.o) and not os.path.exists(os.path.dirname(args.o)):
         os.makedirs(os.path.dirname(args.o))
-    open(args.o, 'w').close()  # create empty file
-    x, y, r = None, None, None  # initial values
 
-    if args.processes:
-        assert args.processes > 0  # I wonder, what happens when an user run the program with -p -2 argument...
-        pool = multiprocessing.Pool(processes=args.processes)
-        iterator = get_frames(args.i)
-        n = True
-        while n:
-            map_args = []
-            for x in range(args.processes):
-                n = next(iterator, False)
-                if n:
-                    map_args.append((n, x, y, r, args.contain, args.solvent, args.main_atom_in_solvent,
-                                     args.skip_hydrogens, args.xtol))
-            for data, lines, x, y, r in pool.map(process_frame_string, map_args):
-                write_file(data.first_line, lines, data.last_line, args.o)
-
-        pool.close()
-        pool.join()
+    if args.figure:
+        first_frame_txt = next(get_frames(args.i))
+        data = DataFrame(first_frame_txt)
+        protein_atoms = np.array(get_protein_atoms(data.lines, False))
+        # determine initial circle parameters
+        initialx = np.mean(protein_atoms[:, 0])
+        initialy = np.mean(protein_atoms[:, 1])
+        initial_radius = (math.fabs(initialx) + math.fabs(initialy) - np.min(protein_atoms[:, 0]) - np.min(protein_atoms[:, 1])) / 2
+        x, y, r = determine_center_and_radius(np.array(protein_atoms), args.xtol, initialx, initialy, initial_radius)
+        plot.plot(protein_atoms, initialx, initialy, initial_radius, x, y, r)#, args.o)
     else:
-        for frame in get_frames(args.i):
-            data = DataFrame(frame)
-            lines, x, y, r = data.process(args.contain, args.solvent, args.main_atom_in_solvent, args.skip_hydrogens,
-                                          args.xtol, x, y, r)
-            write_file(data.first_line, lines, data.last_line, args.o)
+        print "Num of cores:", 1 + args.processes
+        if os.path.dirname(args.o) and not os.path.exists(os.path.dirname(args.o)):
+            os.makedirs(os.path.dirname(args.o))
+        open(args.o, 'w').close()  # create empty file
+        x, y, r = None, None, None  # initial values
+
+        if args.processes:
+            assert args.processes > 0  # I wonder, what happens when an user run the program with -p -2 argument...
+            pool = multiprocessing.Pool(processes=args.processes)
+            iterator = get_frames(args.i)
+            n = True
+            while n:
+                map_args = []
+                for x in range(args.processes):
+                    n = next(iterator, False)
+                    if n:
+                        map_args.append((n, x, y, r, args.contain, args.solvent, args.main_atom_in_solvent,
+                                         args.skip_hydrogens, args.xtol))
+                for data, lines, x, y, r in pool.map(process_frame_string, map_args):
+                    write_file(data.first_line, lines, data.last_line, args.o)
+
+            pool.close()
+            pool.join()
+        else:
+            for frame in get_frames(args.i):
+                data = DataFrame(frame)
+                lines, x, y, r = data.process(args.contain, args.solvent, args.main_atom_in_solvent, args.skip_hydrogens,
+                                              args.xtol, x, y, r)
+                write_file(data.first_line, lines, data.last_line, args.o)
+    print "Result written to", args.o
 
 if __name__ == '__main__':
-# todo opcja z obejrzeniem nanodysku
     main()
--- bozenka_walec/plot.py
+++ bozenka_walec/plot.py
@@ -2,15 +2,24 @@
 # -*- coding: utf-8 -*-
 
 from matplotlib import pyplot as plt
-def plot():
-    circle = plt.Circle((res1.x[0], res1.x[1]), res1.x[2], fill=False, edgecolor='r', label='squared')
-    circle2 = plt.Circle((res2.x[0], res2.x[1]), res2.x[2], fill=False, edgecolor='g', label='not squared')
-    circle3 = plt.Circle((initialx, initialy), initial_radius, fill=False, edgecolor='c', label='before optimisation')
+def plot(points, initialx, initialy, initial_radius, x, y, radius): #TODO outfile
+    """
+    Plots nanodisk boundaries before and after fitting a circle to the boundaries
+    :param points: boundaries (protein) atoms
+    :param initialx: guessed x coordinate of the middle of the circle
+    :param initialy: guessed y coordinate of the middle of the circle
+    :param initial_radius: guessed radius of the circle
+    :param x: final coordinate of the middle of the circle
+    :param y: final coordinate of the middle of the circle
+    :param radius: final radius of the circle
+    :return:
+    """
+    circle = plt.Circle((x, y), radius, fill=False, edgecolor='r', label='after optimization')
+    circle2 = plt.Circle((initialx, initialy), initial_radius, fill=False, edgecolor='c', label='before optimization')
     fig, ax = plt.subplots()
     plt.axis([0, 16, 0, 16])
     ax.add_artist(circle)
     ax.add_artist(circle2)
-    ax.add_artist(circle3)
     orig, = plt.plot(points[:,0], points[:,1], c='y', label='original data')
-    ax.legend(handles=[circle, circle2, circle3, orig])
+    ax.legend(handles=[circle, circle2, orig])
     plt.show()
